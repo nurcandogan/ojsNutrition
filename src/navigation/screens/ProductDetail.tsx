a5b71f4b56096a5135782e67e2c5e9a4f4b0c695
Nurcan Dogan <nurcan.d00@gmail.com>
2025-10-17 17:32:39 +0300

    page ProductDetail is changing

--- src/navigation/screens/ProductDetail.tsx
+++ src/navigation/screens/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React, { use, useState } from 'react'
+import React, { use, useMemo, useState } from 'react'
 import { useRoute } from '@react-navigation/native';
 import { Variant } from '../services/productService';
 import { MiniProduct } from '../../storage-helper/recentlyViewed';
@@ -9,9 +9,21 @@ const ProductDetail = () => {
   const { slug, name } = route.params as { slug: string; name?: string }; 
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
-  const [selectedAroma, setSelectedAroma] = useState<string | null>(null);
-  const [selectedVariant, setSelectedVariant] = useState<Variant | null>(null);
-  const [recent, setRecent] = useState<MiniProduct[]>([]);
+  const [selectedAroma, setSelectedAroma] = useState<string | null>(null);        //Seçilen aroma
+  const [selectedVariant, setSelectedVariant] = useState<Variant | null>(null);   //Seçilen variant-boyut
+  const [recent, setRecent] = useState<MiniProduct[]>([]);   //Son görüntülenenler
+
+ const aromas = useMemo(() => 
+   [...new Set(( data?.variants ?? []).map((v: Variant) => v.aroma ?? 'Aromasız'))], 
+   [data]
+ );
+
+ const sizeOptionsForAroma= useMemo(() => {
+  if (!data) return [];
+  return (data.variants ?? []).filter((v: Variant) => (v.aroma ?? 'Aromasız') === (selectedAroma ?? 'Aromasız'));
+ }, 
+ [data, selectedAroma]); 
+
 
 
   return (
