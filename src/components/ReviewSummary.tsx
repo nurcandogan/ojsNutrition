16888b85b699a6cdde73831333f82844e41598bd
Nurcan Dogan <nurcan.d00@gmail.com>
2025-10-26 21:41:57 +0300

    ..

--- src/components/ReviewSummary.tsx
+++ src/components/ReviewSummary.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from 'react-native'
 import React, { use, useMemo } from 'react'
 import { CommentItem } from '../navigation/services/commentsService';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import RatingBar from './RatingBar';
 
 type Props = {
   comments: CommentItem[];
@@ -51,10 +53,54 @@ return calcAverage(safeComments) || 0;
   // toplam yorum sayısı (16 gibi)
 const total = totalCount ?? safeComments.length ?? 0;
 
+const distribution = useMemo(() => 
+ makeDistribution(safeComments), 
+[safeComments]
+);
+
+
+const totalPage = Math.max(1 , Math.ceil
+  ((total || 0) / (pageSize || 1))
+
+);
+
 
   return (
     <View>
-      <Text>ReviewSummary</Text>
+      <Text className='text-2xl text-center'>{avg.toFixed(1)}</Text>
+      
+      {/* Yıldız ikonları */}
+      <View className="flex-row mt-2 items-center justify-center">
+        {[...Array(5)].map((_, i) => (
+          <AntDesign
+            key={i}
+            name={i < Math.floor(avg) ? 'star' : 'staro'}
+            size={18}
+            color="#FDD835"
+          />
+        ))}
+      </View>
+
+      <Text className='text-base items-center text-center mt-2'> {total} YORUM </Text>
+
+
+            {/* Bar dağılımı */}
+
+      <View className='mt-4'>
+        {[5,4,3,2,1].map((star, idx) => (
+          
+            <View key={star} className='flex-row items-center mb-1 mx-4'>
+              <Text className='w-4 text-sm'>{star}</Text>
+              <AntDesign name='star' size={12} color="#FDD835" />
+               <View className='flex-1 mx-2'>
+                 <RatingBar value={distribution[idx]} total={Math.max(1, total)}/>
+               </View>
+              <Text className='w-6 text-s '>{distribution[idx] ?? 0}</Text>
+            </View>
+        ))}
+      </View>
+
+
     </View>
   )
 }
