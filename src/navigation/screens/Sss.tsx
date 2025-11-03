12eb757abc15aaafc589bbc01ec78071d50773cd
Nurcan Dogan <nurcan.d00@gmail.com>
2025-11-03 17:02:38 +0300

    Sss is being written

--- src/navigation/screens/Sss.tsx
+++ src/navigation/screens/Sss.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { Category } from '../../data/sssData'
 import { ScrollView } from 'react-native-gesture-handler'
@@ -16,10 +16,28 @@ const Sss = () => {
   return (
    <SafeAreaView className='bg-white flex-1'>
     <ScrollView>
-      <View>
-       <BackHeader onPress={() => navigation.goBack()} title=" S.S.S."/>
-        
-      </View>
+      <BackHeader onPress={() => navigation.goBack()} title=" S.S.S."/>
+
+     <View className='mx-6 mt-10 mb-4 '>
+       <View className='flex-row space-x-2 gap-4 '>
+       {tabs.map((tab) => {
+        const selected = tab === activeTab;
+        return(
+          <TouchableOpacity
+           key={tab}
+           onPress={() => {setActiveTab(tab); setOpenId(null) }} 
+           className={`px-4 py-3  h-[50px] w-auto justify-center items-center border ${selected ? 'bg-black ' : ' bg-bordergray border-bordergray' }`}>
+            <Text className= {`${selected ? 'text-white ' : 'text-black'}`}>
+              {tab}
+            </Text>
+          </TouchableOpacity>
+
+        )
+       })}
+       </View>
+     </View>
+     
+
     </ScrollView>
    </SafeAreaView>
   )
