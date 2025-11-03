9fa0c75f63f17ed582efd13965565bc1872ff78b
Nurcan Dogan <nurcan.d00@gmail.com>
2025-11-03 19:18:54 +0300

    some change

--- src/components/TabsMenu/AccordionItem.tsx
+++ src/components/TabsMenu/AccordionItem.tsx
@@ -16,14 +16,16 @@ const AccordionItem: React.FC<Props> = ({ question, answer, isOpen, onPress }) =
         onPress={onPress}
         className="flex-row justify-between items-center px-4 py-5"
         activeOpacity={0.6}
-        
       >
         <Text className="font-bold text-[14.18px] flex-1 mr-2 ">{question}</Text>
-        <Text className="text-[18px]">{isOpen ? '-' : '+'}</Text>
+        <View className="w-[12.5px] h-[12.5px]  border-[1px]   items-center justify-center ">
+             <Text className="text-[10px] absolute ">{isOpen  ? '-'  : '+'}</Text>
+        </View>
+       
       </TouchableOpacity>
 
       {isOpen && (
-        <View className="px-4 pb-5 pt-1">
+        <View className="px-4 pb-5 pt-1 ">
           <Text className="text-[12px] text-black">{answer}</Text>
         </View>
       )}
