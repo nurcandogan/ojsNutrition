97cdd2404ef6f4cae4ec6a0d5cff1eb288dd3a55
Nurcan Dogan <nurcan.d00@gmail.com>
2025-11-21 17:56:06 +0300

    Refactor allProductsService: update AllProduct interface structure and fix fetchAllProduct function syntax

--- src/navigation/services/allProductsService.ts
+++ src/navigation/services/allProductsService.ts
@@ -1,11 +1,24 @@
+import { API_BASE_URL } from "@env";
 
-interface AllProduct {
-	
+export interface AllProduct {
+  id: string;
+  name: string;
+  short_explanation?: string;
+  slug: string;
+  comment_count: number;
+  average_star: number;
+  photo_src: string;
+  price_info: {
+    profit?: number | null;
+    total_price: number;
+    discounted_price: number | null;
+    discount_percentage: number | null;
+  };
 }
 
-export const fetchAllProduct = async (limit: number = 100, offset: number = 0) => {
+export const fetchAllProduct = async (limit: number = 100, offset: number = 0):Promise<AllProduct[]> => {
 	try {
-		const response = await fetch(`$(API_BASE_URL)/products?limit=${limit}&offset=${offset}`);
+		const response = await fetch(`${API_BASE_URL}/products?limit=${limit}&offset=${offset}`);
 		const json = await response.json()
 			return json?.data?.results ?? [];
 	
