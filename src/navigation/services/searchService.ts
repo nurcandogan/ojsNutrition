631db4aae40dbaa6fce1de339107dadf1f12034e
Nurcan Dogan <nurcan.d00@gmail.com>
2025-11-21 20:55:27 +0300

    --

---
+++ src/navigation/services/searchService.ts
@@ -0,0 +1,16 @@
+import { API_BASE_URL } from "@env";
+
+export const searchProducts = async (keyword: string) => {
+  if (!keyword.trim()) return [];
+
+  try {
+    const res = await fetch(
+      `${API_BASE_URL}/products?search=${keyword}&limit=20&offset=0`
+    );
+    const json = await res.json();
+    return json?.data?.results ?? [];
+  } catch (err) {
+    console.log("Search Hatası:", err);
+    return [];
+  }
+};
